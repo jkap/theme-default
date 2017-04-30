7a73db00b1f904bcfd73322e9f63ad2b52fee8b2
jae kaplan <me@jkap.io>
2017-04-30 10:50:39 -0700

    changed fish to lambda

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -13,6 +13,7 @@ function fish_prompt
   end
 
   set -l fish     "⋊>"
+  set -l lambda   "λ"
   set -l ahead    "↑"
   set -l behind   "↓"
   set -l diverged "⥄ "
@@ -20,15 +21,15 @@ function fish_prompt
   set -l none     "◦"
 
   set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
+  set -l success_color    (set_color cyan)
   set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
   set -l directory_color  (set_color $fish_color_quote ^/dev/null; or set_color brown)
   set -l repository_color (set_color $fish_color_cwd ^/dev/null; or set_color green)
 
   if test $last_command_status -eq 0
-    echo -n -s $success_color $fish $normal_color
+    echo -n -s $success_color $lambda $normal_color
   else
-    echo -n -s $error_color $fish $normal_color
+    echo -n -s $error_color $lambda $normal_color
   end
 
   if git_is_repo
